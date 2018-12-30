77f7df52c0df6d96dd3ebb26630b4afab0c74711
Charlotte Patola <charlotte.patola@helsinki.fi>
2018-12-30 16:37:29 +0200

    Fire and hire

--- src/main/java/CompanyStructure/Main.java
+++ src/main/java/CompanyStructure/Main.java
@@ -10,24 +10,27 @@ public class Main {
         System.out.println(Kalle);
 
         Boss Bosse = new Boss("Bosse", 1966, Team.FINLAND, "country manager finland");
-        Bosse.addEmployee(Kallena);
-        Bosse.addEmployee(Kalle);
-        Bosse.addEmployee(Kalles);
+        Bosse.hire(Kallena);
+        Bosse.hire(Kalle);
+        Bosse.hire(Kalles);
         System.out.println(Bosse.toString());
 
         Individual Manda = new Individual("Manda", 1988, "seller");
         Individual Madde = new Individual("Madde", 1989, "marketing coordinater");
         Boss Bosselina = new Boss("Bosselina", 1977, Team.SWEDEN, "country manager sweden");
-        Bosselina.addEmployee(Madde);
-        Bosselina.addEmployee(Manda);
+        Bosselina.hire(Madde);
+        Bosselina.hire(Manda);
         System.out.println(Bosselina.toString());
 
         Company coolcompany = new Company("CoolCompany", "Amsterdam", AccountingStandard.IFRS);
         coolcompany.addBoss(Bosse);
         coolcompany.addBoss(Bosselina);
-        System.out.println( coolcompany.toString());
-       
+        System.out.println(coolcompany.toString());
 
+        Bosse.fire(Kalle);
+        System.out.println(Bosse.toString());
+        System.out.println(Kalle.toString());
+        System.out.println(coolcompany.toString());
     }
 
 }
