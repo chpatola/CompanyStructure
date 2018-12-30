d6706b64f208f68c003a5231d8993e5b16f0a797
Charlotte Patola <charlotte.patola@helsinki.fi>
2018-12-30 08:17:03 +0200

    Complete

--- src/main/java/CompanyStructure/Company.java
+++ src/main/java/CompanyStructure/Company.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CompanyStructure;
 
-/**
- *
- * @author chpatola
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Company {
-    
+
+    private String name;
+    private String location;
+    private AccountingStandard AccoutSt;
+    private List<Boss> bosses;
+
+    public Company(String name, String location, AccountingStandard AccoutSt) {
+        this.name = name;
+        this.location = location;
+        this.AccoutSt = AccoutSt;
+        this.bosses = new ArrayList<>();
+
+    }
+
+    public void addBoss(Boss boss){
+        bosses.add(boss);
+    }
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {  
+        return name + ", " + location + ", " + AccoutSt + ", " + AmountofEmployees() + " employees.";
+    }
+
+    private String AmountofEmployees() {
+        String help = "";
+        int bossAmount = bosses.size();
+        int totSum = bossAmount;
+        for (Boss bossen : bosses) {
+            totSum = totSum + bossen.amountofEmployees();
+
+        }
+        return help +totSum;
+    }
+
 }
