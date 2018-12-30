709e16471ff2acf8c8767e5623262825a870782e
Charlotte Patola <charlotte.patola@helsinki.fi>
2018-12-30 07:43:26 +0200

    Main Structure

---
+++ src/main/java/CompanyStructure/Main.java
@@ -0,0 +1,15 @@
+
+package CompanyStructure;
+
+public class Main {
+    public static void main(String[] args) {
+        Individual Kalle = new Individual("kalle", 1987, "Seller");
+        Individual Kalles = new Individual("kalles", 1989, "Customer Service rep");
+        Individual Kallena = new Individual("kallena", 1983, "Seles Assistant");
+        
+        System.out.println(Kalle);
+        
+        
+    }
+    
+}
